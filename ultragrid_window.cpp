b4887f116e8a7e37f38306208ad0fd49f8469b36
Martin Piatka <445597@mail.muni.cz>
2017-08-08 14:05:22 +0200

    fire

--- ultragrid_window.cpp
+++ ultragrid_window.cpp
@@ -32,7 +32,7 @@ UltragridWindow::UltragridWindow(QWidget *parent): QMainWindow(parent){
 	connect(ui.arguments, SIGNAL(textChanged(const QString &)), this, SLOT(editArgs(const QString &)));
 	connect(ui.editCheckBox, SIGNAL(toggled(bool)), this, SLOT(setArgs()));
 	connect(ui.actionRefresh, SIGNAL(triggered()), this, SLOT(queryOpts()));
-	connect(ui.networkAdvancedCheckBox, SIGNAL(toggled(bool)), this, SLOT(setAdvanced(bool)));
+	//connect(ui.networkAdvancedCheckBox, SIGNAL(toggled(bool)), this, SLOT(setAdvanced(bool)));
 
 	opts.emplace_back(new SourceOption(ui.videoSourceComboBox,
 				ui.videoModeComboBox,
